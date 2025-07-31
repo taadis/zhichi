b34bf50c07ca2e87955ef01290ab1b923b1165c0
taadis <i@taadis.com>
2025-07-31 20:46:07 +0800

    add base request func

--- core/core.go
+++ core/core.go
@@ -1,6 +1,9 @@
 package core
 
-import "net/url"
+import (
+	"encoding/json"
+	"net/url"
+)
 
 // see more: https://developer.zhichi.com/pages/6cc489/
 const (
@@ -9,6 +12,30 @@ const (
 	// 腾讯云环境
 	SOBOT_TENCENT = "www.soboten.com"
 )
+
+type BaseRequest struct{}
+
+func (r *BaseRequest) String() string {
+	if r == nil {
+		return ""
+	}
+	bs, err := json.Marshal(r)
+	if err != nil {
+		return ""
+	}
+	return string(bs)
+}
+
+func (r *BaseRequest) MarshalIndent() string {
+	if r == nil {
+		return ""
+	}
+	bs, err := json.MarshalIndent(r, "", "  ")
+	if err != nil {
+		return ""
+	}
+	return string(bs)
+}
 
 type BaseResponse struct {
 	//
@@ -21,6 +48,17 @@ type BaseResponse struct {
 	RetMsg string `json:"ret_msg,omitempty"`
 	// 返回数据
 	Item interface{} `json:"item,omitempty"`
+}
+
+func (r *BaseResponse) String() string {
+	if r == nil {
+		return ""
+	}
+	bs, err := json.Marshal(r)
+	if err != nil {
+		return ""
+	}
+	return string(bs)
 }
 
 func GetUri(host string, path string) *url.URL {
